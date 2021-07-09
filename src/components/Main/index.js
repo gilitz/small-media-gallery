6b03edf357d68b71064b7b73fb2e943076616e80
gilitz <gil.itzhaky@gmail.com>
2021-07-10 00:03:54 +0300

    cleanup

---
+++ src/components/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+const Main = () => {
+    return (
+        <div className="main">
+            This is main page
+        </div>
+    );
+};
+
+export default Main;
